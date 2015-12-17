87e7a5fe27f8c11151bfe5cd5badeb8413630a96
Kiran Manda <kiran.manda@gmail.com>
2015-12-17 07:59:44 -0800

    Sign up js file

--- html/kiran/registration/signup.js
+++ html/kiran/registration/signup.js
@@ -44,13 +44,46 @@
 	}
 
 	/**
+	* Returns true if the specified email is
+	* a valid email. false otherwise.
+	*/
+	function isValidEmail(email){
+		//TODO
+		return true;
+	}
+
+	/**
+	* Appends the error html for the specfied jQuery Object
+	* and shows the specfied message.
+	*/
+	function showError($obj, message){
+		$obj.addClass("error").closest('td').append('<span class="errorMsg">'+message+'</span>');
+	}
+
+	/**
 	* Validate the specifed jquery Object(form) to 
 	* see all required fields are filled out if not show 
 	* error "Required"
 	**/
 	function validateForm($form){
-		console.log($form);
-		//TODO
+		var success = true;
+
+		$form.find('input[type="text"], input[type="password"]')
+			.each(function() {
+				var val = $(this).val();
+				$(this).removeClass("error").closest('td').find('span.errorMsg').remove();
+				if(val === '') {
+					success = false;
+					showError($(this),'Required');
+				} else {
+					if( $(this).attr('name') === 'email'){
+						if(!isValidEmail(val)){
+							showError($(this), 'Invalid Email');
+						}
+					}
+				}
+			});
+		return success;
 	}
 
 	/**
@@ -68,8 +101,18 @@
 
 	$("form").on("submit", function(e){
 		e.preventDefault();
-		validateForm($(this));
+		var success = validateForm($(this));
+		if(success){
+			console.log("Validation successful");
+			//TODO something
+		}else{
+			console.log("Validation failed");
+		}
 	})
+
+	$('input[type="text"], input[type="password"]').on('keyup blur', function(){
+		validateForm($(this).closest("form"));
+	});
 
 
 })();
