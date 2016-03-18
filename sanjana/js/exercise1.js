23d4bbc2d982b20c2ccd3dd85603cb4f522cc91d
sanjanak <sanjanaleo91@gmail.com>
2016-03-18 02:23:50 -0400

    created functions to run unit tests using mocha

--- sanjana/js/exercise1.js
+++ sanjana/js/exercise1.js
@@ -1,37 +1,50 @@
-// Your js goes here
-//numbersToPrint is 5: 0,1,1,2,3
-//numbersToPrint is 1: 0
-//numbersToPrint is 2: 0,1
-//numbersToPrint is undefined: Please specify a integer > 0
-
-function getFibonacci(){
-        var value = document.getElementById("value").value;//get value from textbox
-        printFibonocci(value);
-        
-}
-function printFibonocci(numbersToPrint){
-  var n1 = 0, n2 = 1, nextnum, outputElement;
-  if(parseInt(numbersToPrint) > 0) 
-  {
-    outputElement = document.getElementById("output"); 
-    outputElement.innerHTML = "";
-    outputElement.innerHTML = "<br/>";
-    outputElement.innerHTML += n1 + "<br/>";
-      while(numbersToPrint >= 2)
-      {       
-        nextnum   = n1 + n2;
-        n1  = n2;
-        n2 = nextnum;
-        outputElement.innerHTML += nextnum + "<br/>";
-         numbersToPrint--; 
+var getFibonacci = function (numbersToPrint){
+  if (parseInt(numbersToPrint) > 0){
+    var i;
+    var f=[];
+    for(i=0; i<numbersToPrint; i++){
+      if(i === 0){
+        f[i] = 0;
+        continue;
+      }else if(i == 1){
+        f[i] = 1;
+        continue;
       }
-    
-  }else if(parseInt(numbersToPrint) <= 0){
-    window.alert("Please enter a value greater than 0 to generate fiboncacci sequence");
-
+      f[i] = f[i-2] + f[i-1];
+    }
+    return f;
+  }else{
+    return  ["You have entered an invalid input. Please specify an integer > 0"];
   }
-  else {
-    window.alert("You have not entered any value. Please enter a value!");
+}
 
-  }
+function getFibonacciSequence(limit){
+  var sequence = '';
+      getFibonacci(limit).forEach(function(value, index){
+        if(isNaN(value)){
+          value = "<div style='color:red'>"+value+"</div>"
+        }
+        sequence = sequence + value + "<br/>"
+      });
+      return sequence;
+}
+
+function printFibonacci(){
+  var output = document.getElementById("output");
+      value = document.getElementById("number").value;
+      sequence = '';
+      // getFibonacci(value).forEach(function(value, index){
+      //   if(isNaN(value)){
+      //     value = "<div style='color:red'>"+value+"</div>"
+      //   }
+      //   sequence = sequence + value + "<br/>"
+      // });
+      //output.innersequence =  "<br/>" + sequence;
+      output.innersequence = "<br/>" + getFibonacciSequence(value);
+}
+
+if (typeof module !== 'undefined' && module.exports != null){
+  exports.getFibonacci = getFibonacci;
+  exports.printFibonacci = printFibonacci;
+  exports.getFibonacciSequence = getFibonacciSequence;
 }
