9a3c64ae74a80aa6ae9c8a5ba869cfee8a75be9a
himakar <himakar.gaddam@gmail.com>
2016-03-18 09:46:45 -0400

    Testing code

--- himakar/js/script.js
+++ himakar/js/script.js
@@ -23,16 +23,45 @@
 	} 
 */
 
-function printFibonocci(){
-	var i;
-	var x = []; // Initializing array
-	x[0] = 0;
-	x[1] = 1;
-	var num;
-	num = document.getElementById("number").value;
-	for(i=0; i<=num; i++)
-	{
-	    x[i] = x[i-2] + x[i-1];
-	    console.log(x[i]+"<br/>");
+function getFibonocci(numbersToPrint){
+	if (parseInt(numbersToPrint)>0) {
+		var i;
+		var x = []; // Initializing array
+		for(i=0; i<=numbersToPrint; i++){
+			if(i===0) {
+				x[0]=0;
+				continue
+			}
+			else if(i===1) {
+				x[1]=1;
+				continue;
+			} 
+			x[i] = x[i-2] + x[i-1];
+		}
+		return x;
 	}
+	else {
+		return ["Specify an integer which is > 0"];
+	}
+}
+
+function printFibonocci(){
+	var output=document.getElementById("output"),
+		value=document.getElementById("number").value,
+		html="",
+		out=document.getElementById("out");
+
+	getFibonocci(value).forEach(function(value,index){
+		if (isNaN(value)){
+			value ="<div style='color:red'>"+value+"</div>"
+		}
+		html=html +value+"<br/>"
+	});
+	out.innerHTML="OUTPUT";
+	output.innerHTML=html;
+}
+
+if (typeof module !=='undefined' && module.exports !==null){
+	exports.getFibonocci=getFibonocci;
+	exports.printFibonocci=printFibonocci;
 }
