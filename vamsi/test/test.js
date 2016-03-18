885d4cea8d6a0091d7eb77fe001e3ef0924a8936
kvsdsubashreddy <kvsdsubashreddy@gmail.com>
2016-03-17 22:59:53 -0400

    commiting the script file

---
+++ vamsi/test/test.js
@@ -0,0 +1,18 @@
+
+var assert = require('assert');
+
+var myClass = require('MyClass');
+
+describe('MyClass', function() {
+
+  describe('myFunction()', function () {
+
+    it('should return something when something is passed', function () {
+
+
+
+    }
+
+  }
+
+}
