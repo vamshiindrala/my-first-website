cb440e618a63b51f7c6c21e0cc5786ab9ca20ab0
kvsdsubashreddy <kvsdsubashreddy@gmail.com>
2016-03-23 01:10:24 -0400

    commiting the calculator file

---
+++ vamsi/js/calculator.js
@@ -0,0 +1,19 @@
+function calculate(getnumber){
+   
+
+}
+
+
+function takeinput(){
+  var input=document.getElementById("input"),
+      value=document.getElementById("number").value,
+      html="";
+
+  calculate(input);
+  input.innerHTML=html + input;
+}
+ function valueeight(){
+   var  value=document.getElementById("value8"),
+   html="";
+   input.innerHTML=html + value8;
+ }
