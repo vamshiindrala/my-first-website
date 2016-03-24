05203a8f55df61c51292df9b1906d21af48924ff
kvsdsubashreddy <kvsdsubashreddy@gmail.com>
2016-03-24 01:35:54 -0400

    commiting the calculator files

--- vamsi/js/calculator.js
+++ vamsi/js/calculator.js
@@ -1,19 +1,53 @@
-function calculate(getnumber){
-   
+ function run7(){
+      document.frm.result.value += "7";
+    }
+    function run8(){
+      document.frm.result.value += "8";
+    }
+    function run9(){
+      document.frm.result.value += "9";
+    }
+    function runplus(){
+      document.frm.result.value += "+";
+    }
+     function run4(){
+      document.frm.result.value += "4";
+    }
+    function run5(){
+      document.frm.result.value += "5";
+    }
+    function run6(){
+      document.frm.result.value += "6";
+    }
+    function runminus(){
+      document.frm.result.value += "-";
+    }
+     function run1(){
+      document.frm.result.value += "1";
+    }
+    function run2(){
+      document.frm.result.value += "2";
+    }
+    function run3(){
+      document.frm.result.value += "3";
+    }
+    function rundivision(){
+      document.frm.result.value += "/";
+    }
+     function run0(){
+      document.frm.result.value += "0";
+    }
+    function rundot(){
+      document.frm.result.value += ".";
+    }
+     function runeval(){
+      var evaluate = eval(document.frm.result.value)
 
-}
-
-
-function takeinput(){
-  var input=document.getElementById("input"),
-      value=document.getElementById("number").value,
-      html="";
-
-  calculate(input);
-  input.innerHTML=html + input;
-}
- function valueeight(){
-   var  value=document.getElementById("value8"),
-   html="";
-   input.innerHTML=html + value8;
- }
+      document.frm.result.value = evaluate;
+    }
+    function runmulti(){
+      document.frm.result.value += "*";
+    }
+    function runclear(){
+      document.frm.result.value = "";
+    }
---
+++ vamsi/test/calctest.js
@@ -0,0 +1,5 @@
+var assert = require('assert');
+var series = require('../js/calculator.js');
+describe('series', function() {
+		
+});
