ecd60a3a0e8a83d0df4b8a1bfac9c968f9f053f9
himakar <himakar.gaddam@gmail.com>
2016-03-24 08:23:10 -0400

    changes to calculator

--- himakar/js/calcscript.js
+++ himakar/js/calcscript.js
@@ -1,9 +1,36 @@
-var keys = document.getElementById('#calculator span');
-var operators = ['+', '-', 'x', '÷'];
-
-for(var i = 0; i < keys.length; i++) {
-	keys[i].onclick = function(in) {
-		// Get the input and button values
-		var input = document.getElementById('.screen');
-		var inputVal = input.innerHTML;
-		var btnVal = this.innerHTML;
+window.onload = function () {
+  var buttons = document.getElementsByTagName('span'),
+      result = document.querySelectorAll('.result p')[0],
+      clear = document.getElementsByClassName('clear')[0];
+  
+  for (var i = 0; i < buttons.length; i += 1) {
+    if (buttons[i].innerHTML === '=') {
+      buttons[i].addEventListener("click", calculate(i));
+    } else {
+      buttons[i].addEventListener("click", addValue(i));
+    }
+  }
+  
+  clear.onclick = function () {
+    result.innerHTML = '';
+  };  
+  
+  function addValue(i) {
+    return function () {
+      if (buttons[i].innerHTML === '÷') {
+         result.innerHTML  += '/';
+      } else if (buttons[i].innerHTML === 'x') {
+         result.innerHTML  += '*';
+      } else {
+                    result.innerHTML += buttons[i].innerHTML;
+            }
+    };
+  }
+  
+  function calculate(i) {
+    return function () {
+            var final_res = result.innerHTML;
+      result.innerHTML = eval(final_res);
+    };
+  }
+};
