f2e562228f310e423dda93d2df352687757a3e1f
vujjinisundeep <vujjinisundeeps@gmail.com>
2016-03-25 00:06:27 -0400

    commiting the modified calcyfile

---
+++ sandy/js/calcy.js
@@ -0,0 +1,79 @@
+ function run7(){
+   document.frm.result.value += "7";
+    }
+    function run8(){
+   document.frm.result.value += "8";
+    }
+    function run9(){
+      document.frm.result.value += "9";
+    }
+    function runplus(){
+      document.frm.result.value += "+";
+    }
+     function run4(){
+      document.frm.result.value += "4";
+    }
+    function run5(){
+      document.frm.result.value += "5";
+    }
+    function run6(){
+      document.frm.result.value += "6";
+    }
+    function runminus(){
+      document.frm.result.value += "-";
+    }
+     function run1(){
+      document.frm.result.value += "1";
+    }
+    function run2(){
+      document.frm.result.value += "2";
+    }
+    function run3(){
+      document.frm.result.value += "3";
+    }
+    function rundivision(){
+      document.frm.result.value += "/";
+    }
+     function run0(){
+      document.frm.result.value += "0";
+    }
+    function rundot(){
+      document.frm.result.value += ".";
+    }
+     function runeval(){
+      var evaluate = eval(document.frm.result.value)
+
+      document.frm.result.value = evaluate;
+    }
+    function runmulti(){
+      document.frm.result.value += "*";
+    }
+    function runclear(){
+      document.frm.result.value = "";
+    }
+
+    var documentarray = [];
+    var sun = document.frm.result.value;
+function calcytest(sun){
+    var StringArray = sun.split("");
+    for(var i=0; i< StringArray.length; i++){
+      if(isNaN(StringArray[i]) === true){
+         if(isNaN(StringArray[i+1] === true)){
+
+          return "Error";
+          break;
+         }
+         else
+          continue;
+      }
+      else
+        continue;
+          }
+  
+
+}
+
+
+ if (typeof module !== 'undefined' && module.exports != null){
+  exports.OperatorSequencetest = OperatorSequencetest;
+  }
