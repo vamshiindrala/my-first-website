2f5675e5cec26c33b5a5edbe6abd95379b7e7d98
kvsdsubashreddy <kvsdsubashreddy@gmail.com>
2016-03-24 23:01:38 -0400

    commiting the calculator files

--- vamsi/js/calculator.js
+++ vamsi/js/calculator.js
@@ -1,8 +1,8 @@
  function run7(){
-      document.frm.result.value += "7";
+   document.frm.result.value += "7";
     }
     function run8(){
-      document.frm.result.value += "8";
+   document.frm.result.value += "8";
     }
     function run9(){
       document.frm.result.value += "9";
@@ -51,3 +51,71 @@
     function runclear(){
       document.frm.result.value = "";
     }
+  //test cases
+    var documentarray = [];
+    var resultString = document.frm.result.value;
+function OperatorSequencetest(resultString){
+    var resultStringArray = resultString.split("");
+    for(var i=0; i< resultStringArray.length; i++){
+      if(isNaN(resultStringArray[i]) === true){
+         if(isNaN(resultStringArray[i+1] === true)){
+
+          return "Enter Valid OPeration";
+          break;
+         }
+         else{
+          continue;
+         }
+      }
+      else{
+        continue;
+      }
+    }
+   
+
+}
+
+
+  /*
+ var operators = ['+','-','*','/']
+ var evalstringarray =[];
+
+  function constructEvalString(keyvalue, operators, evalstringarray){
+    evalstringarray = evalstringarray || [];
+    if(evalstringarray.length > 0){
+      if(isTokenAnOperator(keyvalue)){
+        var previousToken= operators[operators.length - 1];
+        if (isTokenAnOperator(previousToken)) {
+          operators.pop();
+        }
+       operators.push(keyvalue);
+    }
+      else{
+        var currentToken = operators[operators.length - 1];
+        currentToken = currentToken + '' + keyvalue;
+        operators.pop().push(currentToken);
+      }
+      else{
+        evalstringarray.push(keyvalue);
+      }
+      return evalstringarray;
+  
+  }
+
+  function isTokenAnOperator(token){
+    for(var i=0; i< operators.length; i++){
+      if(operators[i] === token){
+        return true;
+      }
+    }
+        return false;
+  }
+
+  if (typeof module !== 'undefined' && module.exports != null){
+  exports.constructEvalString = constructEvalString;
+  exports.isTokenAnOperator = isTokenAnOperator;
+}
+*/
+ if (typeof module !== 'undefined' && module.exports != null){
+  exports.OperatorSequencetest = OperatorSequencetest;
+  }
