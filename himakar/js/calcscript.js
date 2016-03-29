35eec204367152881c347d746988be674890f06d
himakar <himakar.gaddam@gmail.com>
2016-03-29 09:48:08 -0400

    Update on Calculator

--- himakar/js/calcscript.js
+++ himakar/js/calcscript.js
@@ -1,3 +1,4 @@
+/*
 window.onload = function () {
   var buttons = document.getElementsByTagName('span'),
       result = document.querySelectorAll('.result p')[0],
@@ -34,3 +35,81 @@ window.onload = function () {
     };
   }
 };
+*/
+
+var evalStringArray = [];
+
+function handleKeyClick(value){
+	if(value === '='){
+		var result = getResult(evalStringArray);
+		evalStringArray = [];
+		document.querySelector('.screen').innerHTML = result;
+		evalStringArray.push(result);
+	}else if(value === 'AC'){
+		evalStringArray = [];
+		document.querySelector('.screen').innerHTML = '';
+	}else{
+		constructEvalString(value, evalStringArray);
+		document.querySelector('.screen').innerHTML = evalStringArray.join(' ');
+	}	
+}
+
+function getResult(evalStringArray) {
+	if(evalStringArray && evalStringArray.length > 0){
+		if(isTokenAnOperator(evalStringArray[0])){
+			evalStringArray.unshift('0');
+		}
+		if(isTokenAnOperator(evalStringArray[evalStringArray.length-1])){
+			evalStringArray.pop();
+		}
+		return eval(evalStringArray.join(''));
+	}
+	return 0;
+}
+
+function constructEvalString(keyvalue, evalStringArray) {
+	var evalStringArray = evalStringArray || [];
+	if(keyvalue) {
+		keyvalue = keyvalue + '';
+		if(evalStringArray.length > 0){
+			if(isTokenAnOperator(keyvalue)){
+				var previousToken = evalStringArray[evalStringArray.length - 1];
+				if(isTokenAnOperator(previousToken)){
+					evalStringArray.pop();
+				}
+				evalStringArray.push(keyvalue);
+			}else{
+				var previousToken = evalStringArray[evalStringArray.length - 1];
+				if(isTokenAnOperator(previousToken)){
+					evalStringArray.push(keyvalue);
+				}else{
+					evalStringArray.pop();
+					evalStringArray.push(previousToken + keyvalue);
+				}
+			}
+		}else{
+			evalStringArray.push(keyvalue);
+		}
+		return evalStringArray;
+	}else{
+		return [];
+	}
+}
+
+
+function isTokenAnOperator(token){
+	var operators = ['+', '-', '*', '/'];
+	for(var i =0; i < operators.length; i++) {
+		if(operators[i] === token){
+			return true;
+		}
+	}
+	return false;
+}
+
+if (typeof module !== 'undefined' && module.exports != null){
+  exports.constructEvalString = constructEvalString;
+  exports.isTokenAnOperator = isTokenAnOperator;
+  exports.handleKeyClick = handleKeyClick;
+  exports.getResult = getResult;
+}
