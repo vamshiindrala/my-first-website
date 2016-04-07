357516c4a05810fb51674c0a640846657962c67f
himakar <himakar.gaddam@gmail.com>
2016-04-07 09:53:18 -0400

    changes to Todo List

--- himakar/js/todo.js
+++ himakar/js/todo.js
@@ -1,38 +1,95 @@
-document.querySelector('#taskInput').addEventListener('keypress', function (e) {
-    var key = e.which || e.keyCode;
-    if (( key == 13 ) && ( document.querySelector('#taskInput').value.length > 0 )) // 13 is enter
-    	addToList( this.value.trim() );
-});
+/*
 
-var taskList = [],
-	completedTasks = [];
+function add(){
 
-if( JSON.parse( localStorage.getItem( 'taskList' )))
-	taskList = JSON.parse( localStorage.getItem( 'taskList' ));
-else
-	localStorage.setItem("taskList", JSON.stringify( taskList ));
+  var array = array || []
+  
+  for(var i=0; i<=array.length; i++){
+    var docvalue = document.getElementById('taskInput').value;
+    var result = array.push(docvalue);
+     console.log( result);
+    document.getElementById('textoutput').innerHTML = array;
+  }
 
-updateCompletedListArray();
-updateListView();
-
-function updateCompletedListArray() {
-	completedTasks = [];
-
-	taskList.forEach(function( task ) {
-		if( task.done )
-			completedTasks.push( taskList.indexOf( task ) + '' );
-	});
 }
 
-function addToList( task ){
-	taskList.push({
-		name: task,
-		done: false
-	});
-
-	updateListView();
-
-	localStorage.setItem('taskList', JSON.stringify( taskList ));
-	document.querySelector('#taskInput').value = '';
+function edit(EditItem){
+    var editarray = array;
+    for(var i=0; i<editarray.length; i++){
+     if(editarray[i] === EditItem){
+          editarray.splice(i, 1, EditItem);
+          break;
+     }
+     else{
+      continue;
+     }
+    }
 }
 
+function Delete(DeleteItem){
+   var editarray = array;
+    for(var i=0; i<editarray.length; i++){
+     if(editarray[i] === DeleteItem){
+          editarray.splice(i, 1);
+          break;
+     }
+     else{
+      continue;
+     }
+    }
+}
+*/
+
+
+ 
+function add() {
+    var task = document.getElementById('task').value;
+ 
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+ 
+    show();
+ 
+    return false;
+}
+ 
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+ 
+    show();
+ 
+    return false;
+}
+ 
+function show() {
+    var todos = get_todos();
+ 
+    var html = '<ul>';
+    for(var i=0; i<todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
+    };
+    html += '</ul>';
+ 
+    document.getElementById('todos').innerHTML = html;
+ 
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
+
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str); 
+    }
+    return todos;
+}
+ 
+document.getElementById('add').addEventListener('click', add);
+show();
