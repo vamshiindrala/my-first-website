1c3319fef4f564a13dae157fc269141bd4b61471
himakar <himakar.gaddam@gmail.com>
2016-04-08 08:11:30 -0400

    changes to todo

--- himakar/js/todo.js
+++ himakar/js/todo.js
@@ -41,7 +41,7 @@ function Delete(DeleteItem){
 */
 
 
- 
+ /*
 function add() {
     var task = document.getElementById('task').value;
  
@@ -93,3 +93,97 @@ function get_todos() {
  
 document.getElementById('add').addEventListener('click', add);
 show();
+*/
+
+var todoList = [];
+
+function addToDo(todoItemTask, todoList) {
+	var todoList = todoList || [],
+		todoItem;
+
+	todoItem = buildToDoItem(todoItemTask);
+	if(todoItem){
+		todoList.push(todoItem);
+		return todoList;
+	}
+	return false;
+}
+
+function buildToDoItem(todoItemTask){
+	if(todoItemTask && typeof todoItemTask === 'string') {
+		var todoItem = {};
+		todoItem.task = todoItemTask;
+		todoItem.status = 'Incomplete';
+		todoItem.date = (new Date()).getTime();
+		return todoItem;
+	}
+	return null;
+}
+
+function removeToDo(index, todoList) {
+	index = parseInt(index);
+	if(todoList && todoList.length > 0 && !isNaN(index) && index >= 0){
+		todoList.splice(index, 1);
+		return todoList;
+	}
+	return false;
+}
+
+function editToDo(index, todoItemTask, todoList) {
+	index = parseInt(index);
+ 	if (todoList && todoList.length > 0 && !isNaN(index) && index >=0) {
+    	todoList.splice(index, 1, todoItemTask);
+    	return todoList;
+ 	}  
+	return false;
+}
+
+function markToDoAsDone(index, todoList) {
+	index = parseInt(index);
+	if (todoList && todoList.length > 0 && !isNaN(index) && index >=0) {
+	for(var i=0; i< todoList.length; i++){
+	if(todoList[i] === todoItemTask){
+		var markDone = todoList[i].style.color = 'red';
+		return markDone;
+	}
+	else break;
+	}
+	}
+	return false;
+}
+
+// function getToDoList() {
+
+// }
+
+// function saveToDoList() {
+
+// }
+
+ 
+function buildListHTML(todoList) {
+	var html = ''; 
+ 	if(todoList && todoList.length > 0){
+	    html = '<ul>';
+	    for(var i=0; i<todoList.length; i++) {
+	    	if(todoList[i].status === 'Done'){
+	    		html += '<li><span class="strike-through">' + todoList[i].task + '</span><button class="remove" id="' + i  + '">x</button></li>';
+	    	}else{
+	    		html += '<li><span>' + todoList[i].task + '</span><button class="remove" id="' + i  + '">x</button></li>';
+	    	}	        
+	    };
+	    html += '</ul>';
+ 	}
+    return html;
+}
+
+if (typeof module !== 'undefined' && module.exports != null){
+  exports.buildToDoItem = buildToDoItem;
+  exports.addToDo = addToDo;
+  exports.removeToDo = removeToDo;
+  exports.editToDo = editToDo;
+  exports.markToDoAsDone = markToDoAsDone;
+  exports.buildListHTML = buildListHTML;
+}
+
+
